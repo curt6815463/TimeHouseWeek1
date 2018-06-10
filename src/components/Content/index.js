f00fc2296665287aa0882f27574cf049890a55da
curt <curt6815463@gmail.com>
2018-06-10 13:35:09 +0800

    add toggleOpening

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -41,6 +41,12 @@ class Content extends Component {
     this.setState({events})
   }
 
+  toggleOpening = (index) => {
+    let events = this.state.events
+    events[index].opening = !events[index].opening
+    this.setState({events})
+  }
+
   render() {
     return (
       <div className="content">
@@ -63,96 +69,103 @@ class Content extends Component {
                       <label htmlFor={'checkbox' + index}>{event.title}</label>
                       <input
                         name="title"
-                        onChange={this.handleChange(index)}                        
+                        onChange={this.handleChange(index)}
                       />
                     </div>
                     <div className="starIcon">
                       <i className="far fa-star"></i>
                     </div>
-                    <div className="pencilIcon">
+                    <div className="pencilIcon" onClick={() => this.toggleOpening(index)}>
                       <i className="fas fa-pencil-alt"></i>
                     </div>
-                    <div className="stateBlock">
-                      <div className="calendarIcon">
-                        <i className="far fa-calendar-alt iconPadding"></i>
-                      {event.deadlineDate}{event.deadlineTime}
+                    {!event.opening &&
+                      <div className="stateBlock">
+                        <div className="calendarIcon">
+                          <i className="far fa-calendar-alt iconPadding"></i>
+                        {event.deadlineDate}{event.deadlineTime}
+                        </div>
+                        <div className="fileIcon">
+                          <i className="far fa-file iconPadding"></i>
+                        </div>
+                        <div className="messageIcon">
+                          <i className="far fa-comment-dots iconPadding"></i>
+                        </div>
                       </div>
-                      <div className="fileIcon">
-                        <i className="far fa-file iconPadding"></i>
-                      </div>
-                      <div className="messageIcon">
-                        <i className="far fa-comment-dots iconPadding"></i>
-                      </div>
-                    </div>
+                    }
                   </div>
 
-                  <div className="cardDetailContainer">
-                    <div className="detail-block calendar">
-                      <div className="icon">
-                        <i className="far fa-calendar-alt"></i>
-                      </div>
-                      <div className="deadline">
-                        <div className="wording">
-                          Deadline
+                  {event.opening &&
+                    <div className="cardDetailContainer">
+                      <div className="detail-block calendar">
+                        <div className="icon">
+                          <i className="far fa-calendar-alt"></i>
                         </div>
-                        <div className="calendarInputBlock">
-                          <input
-                            name="deadlineDate"
-                            value={event.deadlineDate}
-                            onChange={this.handleChange(index)}
-                          />
-                          <input
-                            name="deadlineTime"
-                            value={event.deadlineTime}
-                            onChange={this.handleChange(index)}
-                          />
-                        </div>
+                        <div className="deadline">
+                          <div className="wording">
+                            Deadline
+                          </div>
+                          <div className="calendarInputBlock">
+                            <input
+                              name="deadlineDate"
+                              value={event.deadlineDate}
+                              onChange={this.handleChange(index)}
+                            />
+                            <input
+                              name="deadlineTime"
+                              value={event.deadlineTime}
+                              onChange={this.handleChange(index)}
+                            />
+                          </div>
 
+                        </div>
+                      </div>
+                      <div className="detail-block file">
+                        <div className="icon">
+                          <i className="far fa-file"></i>
+                        </div>
+                        <div className="file">
+                          <div className="wording">
+                            File
+                          </div>
+                          <div className="addFileIcon">
+                            <i className="fas fa-plus-square"></i>
+                            {event.file}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="detail-block comment">
+                        <div className="icon">
+                          <i className="far fa-comment-dots"></i>
+                        </div>
+                        <div className="comment">
+                          <div className="wording">
+                            Comment
+                          </div>
+                          <div className="commentInput">
+                            <textarea
+                              name="comment"
+                              value={event.comment}
+                              onChange={this.handleChange(index)}
+                            ></textarea>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="detail-block file">
-                      <div className="icon">
-                        <i className="far fa-file"></i>
-                      </div>
-                      <div className="file">
-                        <div className="wording">
-                          File
-                        </div>
-                        <div className="addFileIcon">
-                          <i className="fas fa-plus-square"></i>
-                          {event.file}
-                        </div>
-                      </div>
-                    </div>
-                    <div className="detail-block comment">
-                      <div className="icon">
-                        <i className="far fa-comment-dots"></i>
-                      </div>
-                      <div className="comment">
-                        <div className="wording">
-                          Comment
-                        </div>
-                        <div className="commentInput">
-                          <textarea
-                            name="comment"
-                            value={event.comment}
-                            onChange={this.handleChange(index)}
-                          ></textarea>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  }
 
-                  <div className="cardBottonBlock">
-                    <div className="cancel">
-                      <i className="fas fa-times"></i>
-                      <span>Cancel</span>
+                  {event.opening &&
+                    <div className="cardBottonBlock">
+                      <div className="cancel">
+                        <i className="fas fa-times"></i>
+                        <span>Cancel</span>
+                      </div>
+                      <div className="save">
+                        <i className="fas fa-plus"></i>
+                        <span>Save</span>
+                      </div>
                     </div>
-                    <div className="save">
-                      <i className="fas fa-plus"></i>
-                      <span>Save</span>
-                    </div>
-                  </div>
+                  }
+
 
                 </div>
 
